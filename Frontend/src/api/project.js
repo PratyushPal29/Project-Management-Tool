6cd7a1d482d8d64bccc422261820b0b0bd8435a3
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-15 01:50:12 +0530

    modified:   Frontend/src/api/project.js

--- Frontend/src/api/project.js
+++ Frontend/src/api/project.js
@@ -21,7 +21,7 @@ export const addProject = async ({ userId, username, projName, projDesc }) => {
             projDesc: projDesc
         });
         
-        console.log("Document written with ID: ", docRef.id);
+        //console.log("Document written with ID: ", docRef.id);
        
     } catch (e) {
         console.error("Error adding document: ", e);
