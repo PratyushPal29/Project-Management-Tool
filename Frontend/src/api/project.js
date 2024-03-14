e25878125362c199912556f5364aa1e53451c831
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-14 13:15:46 +0530

    modified:   Frontend/src/api/project.js

--- Frontend/src/api/project.js
+++ Frontend/src/api/project.js
@@ -1,11 +1,11 @@
-const { collection, addDoc } = require("firebase/firestore");
+const { collection, addDoc,setDoc } = require("firebase/firestore");
 const { db } = require("../firebase/firebase");
 import { useAuth } from '../contexts'
 const getProject = async ({ projName, projdesc }) => {
-    const { currentUser } = useAuth()
+    const { currentUser } = useAuth();
     try {
         const docRef = await addDoc(collection(db, "user-projects"), {
-            user: currentUser,
+            user: currentUser.displayName,
             projName: projName,
             projdesc: projdesc
         });
@@ -13,6 +13,7 @@ const getProject = async ({ projName, projdesc }) => {
     } catch (e) {
         console.error("Error adding document: ", e);
     }
+    //await setDoc(doc(db, "user-projects"), docRef);
 };
 
 
