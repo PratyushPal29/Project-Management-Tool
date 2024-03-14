b62856aab439d216abc4002f83460d84dbd42fe7
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-14 13:04:09 +0530

    new file:   Frontend/.env 	modified:   Frontend/src/api/project.js 	modified:   Frontend/src/firebase/auth.js

--- Frontend/src/api/project.js
+++ Frontend/src/api/project.js
@@ -1,17 +1,18 @@
 const { collection, addDoc } = require("firebase/firestore");
 const { db } = require("../firebase/firebase");
-
-const getProject = async () => {
-  try {
-    const docRef = await addDoc(collection(db, "user-projects"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+import { useAuth } from '../contexts'
+const getProject = async ({ projName, projdesc }) => {
+    const { currentUser } = useAuth()
+    try {
+        const docRef = await addDoc(collection(db, "user-projects"), {
+            user: currentUser,
+            projName: projName,
+            projdesc: projdesc
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 };
 
 
