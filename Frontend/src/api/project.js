4972319cb29eea5b769f0ce935103ce898c397d5
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-14 13:29:02 +0530

    modified:   Frontend/src/api/project.js 	new file:   Frontend/src/api/user.js

--- Frontend/src/api/project.js
+++ Frontend/src/api/project.js
@@ -1,7 +1,7 @@
-const { collection, addDoc,setDoc } = require("firebase/firestore");
+const { collection, addDoc, setDoc, getDoc, updateDoc } = require("firebase/firestore");
 const { db } = require("../firebase/firebase");
 import { useAuth } from '../contexts'
-const getProject = async ({ projName, projdesc }) => {
+const addProject = async ({ projName, projdesc }) => {
     const { currentUser } = useAuth();
     try {
         const docRef = await addDoc(collection(db, "user-projects"), {
@@ -16,4 +16,29 @@ const getProject = async ({ projName, projdesc }) => {
     //await setDoc(doc(db, "user-projects"), docRef);
 };
 
+const getProject = async () => {
+    const docRef = doc(db, "user-projects");
+    const docSnap = await getDoc(docRef);
 
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}
+
+const updateProject = async ({ projName, projdesc }) => {
+    const { currentUser } = useAuth();
+    try {
+        const docRef = await updateDoc(collection(db, "user-projects"), {
+            user: currentUser.displayName,
+            projName: projName,
+            projdesc: projdesc
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+    //await setDoc(doc(db, "user-projects"), docRef);
+};
