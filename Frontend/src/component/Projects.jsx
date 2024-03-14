c94cc45f84c8629c627a1197606e83492eb8bb35
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-14 17:06:21 +0530

    modified:   Frontend/src/component/Projects.jsx

--- Frontend/src/component/Projects.jsx
+++ Frontend/src/component/Projects.jsx
@@ -13,7 +13,20 @@ export default function MediaCard() {
             <div>
                 <h1>Projects</h1>
             </div>
-            <Card sx={{ maxWidth: 345, borderRadius: "20px", border: "2px solid", margin: "2vh" }}>
+            <Card sx={{ maxWidth: 345, borderRadius: "20px", border: "2px solid teal", margin: "2vh" }}>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Name
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" justifyContent='space-between'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque quam officia porro quidem cum obcaecati velit corrupti repellat nostrum dolores nisi temporibus nobis illo, alias a. Libero esse minima distinctio?
+                    </Typography>
+                </CardContent>
+                <CardActions style={{alignContent: "center"}}>
+                    <Button style={{ borderRadius: "20px", backgroundColor: "teal" }}><Link style={{ textDecoration: "none", color: "white" }}>Open</Link></Button>
+                </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 345, borderRadius: "20px", border: "2px solid teal", margin: "2vh" }}>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         Name
